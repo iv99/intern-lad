f9183266d005ef01192766eb31558644b3a71c74
iv99 <upucka92@ya.ru>
2022-08-13 17:13:09 +0300

    task3

--- task3/index.js
+++ task3/index.js
@@ -1,5 +1,54 @@
 const rls = require('readline-sync');
-rls.setDefaultOptions({encoding: 'utf8'});
-const userName = rls.question('Как тебя зовут? ');
 
-console.log('Привет', userName);
+let stepsNum = 10;
+let secNum = '' + Math.ceil(Math.random() * (99999 - 100) + 100);
+console.log(`Угадай число из ${secNum.length} цифр, у вас ${stepsNum} попыток`);
+
+//
+console.log('Загаданное число: ',secNum);
+//
+for(let step = 1; step < stepsNum + 1; step++){
+    console.log(`Попытка ${step}`);
+    console.log('Введите число: ');
+    let ans = rls.question();
+    console.log('Введенное число: ',ans);
+    if (checkNum(ans,secNum)){
+        console.log('Поздравляем! Вы угадали!')
+        break;
+    }else{
+        console.log('Попробуйте еще раз')
+    }
+
+}
+console.log('Загаданное число: ',secNum);
+
+function checkNum(secNum,ansNum){
+    if(secNum == ansNum){
+        return true; //число угадано
+    }else{
+        if(secNum.length == ansNum.length){
+            let bulls = [];
+            let cows = [];
+            for(let i = 0; i < secNum.length;i++){
+                if(secNum[i] == ansNum[i]){
+                    //цифра угадана
+                    bulls.push(ansNum[i])
+                    continue
+                }else if(secNum.indexOf(ansNum[i]) != -1){
+                    //цифра есть в числе
+                    cows.push(ansNum[i]);
+                }
+            }
+            console.log('совпавших цифр не на своих местах: ',cows.length,` (${cows.join(' ')})`);
+            console.log('цифр на своих местах: ',bulls.length,` (${bulls.join(' ')})`);
+
+        }else{
+            console.log('Количество разрядов числа не совпадает с загаданным числом')
+            return false;
+        }
+    }
+
+}
+
+
+//const asd = rls.question();
